520f8cf625c4902c25568ad289ae76b8b6c7b638
phihung0131 <phihung0131@gmail.com>
2024-11-19 21:54:22 +0700

    sửa .env

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
-
+// XXXX
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
